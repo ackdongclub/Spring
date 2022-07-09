930c5b3bf97fe3a5bfad52d6c8020584d216a53d
ackdongclug <afreshbreeze21@naver.com>
2022-07-09 12:40:45 +0900

    memberService

---
+++ src/main/java/hello/core/member/MemoryMemberRepositoryImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepositoryImpl implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //component Haspmap 사용해야 한다 동시성 이슈 발생때매 근데 여기선 안함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
