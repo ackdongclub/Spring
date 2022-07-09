8138b385f08e6908fda2b93623815b6292915c96
ackdongclug <afreshbreeze21@naver.com>
2022-07-09 13:32:35 +0900

    discountService

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,7 +3,7 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepositoryImpl implements MemberRepository {
+public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); //component Haspmap 사용해야 한다 동시성 이슈 발생때매 근데 여기선 안함
 
