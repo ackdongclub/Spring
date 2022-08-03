38656b280bf7ea6e1aff2c01d3a8e7f3aa13f236
ackdongclub <afreshbreeze21@naver.com>
2022-08-03 10:07:21 +0900

    Configuration이 없으면 bean을 ioc 컨테이너에 넣을 순 있지만 싱글톤 보장이 인된다 그래서 설정정보가 있는 곳에는 거으 모두 Configuration을 붙여준다

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,26 +11,43 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//여기에서 객체 생성과 연결을 담당한다
-//MemberserviceImpl/ memoryMemberRepository도 생성해서 연결 시켜준다
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()  > 각각 다른 객체 2개 생성이 된다
+    //singleton이 깨지는 거 아닝가
 
-@Configuration //구성정보
+
+
+    //여기에서 객체 생성과 연결을 담당한다
+    //MemberserviceImpl/ memoryMemberRepository도 생성해서 연결 시켜준다
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
+
+@Configuration //설정정보고 았는 곳에 모두 붙이면 싱글톤 보장 가능
 public class AppConfig {
     //스프링 빈 조회 시 동일한 타입이 둘 이상일 경우 에러남 > 빈 이름 지정
     //
     @Bean //각메써드에 Bean을 적어주면 spring container에 해당 매써드가 등록됨
     public MemberService memberService() {
-
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
     @Bean
     public  MemoryMemberRepository memberRepository() {
-
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
-
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
